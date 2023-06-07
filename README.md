33d85a4f2af29cda99bb987fc39520df5bde9897
ahlam <ahlamalsaffarini@gmail.com>
2023-06-08 01:27:20 +0300

    upload the files to git hub

---
+++ README.md
@@ -0,0 +1,48 @@
+<h1 align="center">article preview component
+</h1>
+
+![](/screenshots/desktop.jpg)
+
+<br>
+
+<div align="center">
+   <a href="https://ahlam-alsaffarini.github.io/FAQ/">Demo </a>
+</div>
+
+<div align="center">
+   Solution for a challenge from  <a href="https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT" target="_blank">frontendmentor.io</a>.
+</div>
+<br>
+<br>
+
+## Overview
+
+### The challenge
+
+This a challenge for the Article preview component coding challenge which user should be able to:
+
+- View the optimal layout for the component depending on their device's screen size
+- See the social media share links when they click the share icon
+- Open the image
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox and grid
+- Mobile-first workflow
+
+### What I learned
+
+ohh a lot of things ,yeah i still bad in some of design creative such as work with js and sizes of each media ,but i hope to become more creative and intelligent ^-^ !
+
+### Useful resources
+
+- [MDN](https://developer.mozilla.org/en-US/)
+- [stackoverflow](https://stackoverflow.com/)
+
+## Acknowledgments
+
+A big thank you to anyone providing feedback on my . It definitely helps to find new ways to code and find easier solutions!
---
+++ js/main.js
@@ -0,0 +1,111 @@
+// make a time for loading page
+setTimeout(() => {
+  document.querySelector(".loading").style.display = "none";
+}, 2000);
+
+//show links when its click
+let share = document.querySelector(".share-icon");
+let shareLink = document.querySelector(".links");
+share.onclick = function (even) {
+  even.stopPropagation();
+  shareLink.classList.toggle("open");
+};
+//click any where
+document.addEventListener("click", (ele) => {
+  if (ele.target !== share && ele.target !== shareLink) {
+    if (shareLink.classList.contains("open")) {
+      shareLink.classList.remove("open");
+    }
+  }
+});
+//stop propagation on links
+shareLink.onclick = function (e) {
+  //stop propogation
+  e.stopPropagation();
+};
+
+// now lets do clicked in link and give the check send then back to normal way
+let links = document.querySelectorAll(".links a");
+
+links.forEach((link) => {
+  link.addEventListener("click", (ele) => {
+    let color = ele.target.parentElement.querySelector(
+      ".loading-share .dot-rotate"
+    ).dataset.color;
+    ele.target.parentElement.querySelector(
+      ".loading-share .dot-rotate"
+    ).style.borderBottomColor = color;
+
+    ele.target.parentElement.querySelector(".fa-check").style.color = color;
+
+    ele.target.parentElement
+      .querySelector(".loading-share")
+      .classList.toggle("active");
+    setTimeout(function () {
+      ele.target.parentElement
+        .querySelector(".loading-share")
+        .classList.remove("active");
+    }, 1000);
+    setTimeout(function () {
+      ele.target.style.display = "none";
+      ele.target.parentElement.querySelector(".fa-check").style.display =
+        "block";
+    }, 1000);
+    setTimeout(function () {
+      ele.target.style.display = "block";
+      ele.target.parentElement.querySelector(".fa-check").style.display =
+        "none";
+    }, 2000);
+  });
+});
+
+//and the last lets try to open the img and make a pop up okay ? -okay
+let gallery = document.querySelector(".main .img img");
+
+gallery.onclick = function (img) {
+  //create overlay element
+  let overlay = document.createElement("div");
+
+  // add class to overlay
+  overlay.classList.add("popup-overlay");
+
+  //append overlay to body
+  document.body.appendChild(overlay);
+
+  //create popup
+  let popupBox = document.createElement("div");
+
+  // add class to popupBox
+  popupBox.classList.add("popup-box");
+  //create img
+  let popupImg = document.createElement("img");
+
+  //set src
+  popupImg.src = img.target.src;
+
+  //add img to pupupBox
+  popupBox.appendChild(popupImg);
+
+  //append popupbox to popup-overlay
+  document.body.appendChild(popupBox);
+
+  //create close span
+  let closeButton = document.createElement("span");
+  closeButton.classList.add("img-close");
+
+  // create close text
+  let closeText = document.createTextNode("X");
+
+  closeButton.appendChild(closeText);
+
+  popupBox.appendChild(closeButton);
+};
+//close popup
+
+document.addEventListener("click", (ele) => {
+  if (ele.target.className == "img-close") {
+    ele.target.parentElement.remove();
+
+    document.querySelector(".popup-overlay").remove();
+  }
+});
